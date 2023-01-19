b45cfdcee8e07fd478056b410bb9ab4f0e106d87
unknown <rishabhkr852@gmail.com>
2023-01-19 14:18:22 +0530

    fixed incomplete HelloSpark file

--- jobs/main.py
+++ jobs/main.py
@@ -2,6 +2,14 @@ from pyspark.sql import *
 
 from logger import Log4j
 
+
+def count_by_country(survey_df):
+    return survey_df.filter("Age < 40") \
+        .select("Age", "Gender", "Country", "state") \
+        .groupBy("Country") \
+        .count()
+
+
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .master("local[3]") \
@@ -18,5 +26,6 @@ if __name__ == "__main__":
         .load("data/sample.csv")
 
     partitioned_df = survey_df.repartition(2)
+    count_df = count_by_country(partitioned_df)
+    count_df.show()
     input("enter a number")
-
